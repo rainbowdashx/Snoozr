395e84f1389c76c8ff386486a870ab3070325de4
rainbowdashx <born_to_kill_666@hotmail.com>
2018-08-31 05:31:35 +0200

    set Bed Times

--- app/src/main/java/com/morf/snoozr/BedTimes.java
+++ app/src/main/java/com/morf/snoozr/BedTimes.java
@@ -1,11 +1,17 @@
 package com.morf.snoozr;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.provider.AlarmClock;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
@@ -15,6 +21,9 @@ public class BedTimes extends AppCompatActivity {
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private String mDateText;
+    private Calendar mCurrentTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,24 +46,60 @@ public class BedTimes extends AppCompatActivity {
         Long StartTimeMs = In.getLongExtra("StartTime", 0);
 
 
-
         //CALCULATE TIMES
-        Calendar currentTime = Calendar.getInstance();
-        currentTime.setTimeInMillis(StartTimeMs);
+        mCurrentTime = Calendar.getInstance();
+        mCurrentTime.setTimeInMillis(StartTimeMs);
 
         List<ContentItem> DataSet = new LinkedList<>();
 
         for (int i = 1; i < 10; i++) {
-            Calendar tmp = (Calendar) currentTime.clone();
+            Calendar tmp = (Calendar) mCurrentTime.clone();
             tmp.add(Calendar.MINUTE, i * 90);
-            ContentItem newItem = new ContentItem(tmp, currentTime);
+            ContentItem newItem = new ContentItem(tmp, mCurrentTime);
 
-            DataSet.add(0,newItem);
+            DataSet.add(0, newItem);
         }
-
-
-        mAdapter = new ListAdapter(DataSet,null,true);
+        mAdapter = new ListAdapter(DataSet, null, true);
 
         mRecyclerView.setAdapter(mAdapter);
+
+
+        Button btnWakeup = findViewById(R.id.btnSetAlarmWakeUp);
+        mDateText = SimpleDateFormat.getTimeInstance(SimpleDateFormat.SHORT).format(mCurrentTime.getTime());
+        String ButtonText = "Set Alarm at: " + mDateText;
+        btnWakeup.setText(ButtonText);
+
+    }
+
+    public void SetAlarmWakeup(View view) {
+
+        AlertDialog.Builder adb = new AlertDialog.Builder(this, R.style.SnoozrAlertDialogStyle);
+        adb.setTitle("Set Alarm for: " + mDateText);
+        adb.setIcon(R.drawable.ic_baseline_alarm_add_24px);
+
+        adb.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                Intent i = new Intent(AlarmClock.ACTION_SET_ALARM);
+                i.putExtra(AlarmClock.EXTRA_MESSAGE, mDateText);
+                i.putExtra(AlarmClock.EXTRA_HOUR, mCurrentTime.get(Calendar.HOUR_OF_DAY));
+                i.putExtra(AlarmClock.EXTRA_MINUTES, mCurrentTime.get(Calendar.MINUTE));
+                i.putExtra(AlarmClock.EXTRA_SKIP_UI, true);
+                startActivity(i);
+
+                finish();
+
+            }
+        });
+
+
+        adb.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        adb.show();
+
+
     }
 }
